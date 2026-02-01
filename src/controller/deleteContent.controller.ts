34e04d09ee21ea605e41aea5cd079528c2909c2a
Anurag Kumar Singh <anuragkrsingh3456@gmail.com>
2026-02-01 17:57:11 +0530

    delete content success

--- src/controller/deleteContent.controller.ts
+++ src/controller/deleteContent.controller.ts
@@ -1,7 +1,29 @@
 import type { Request, Response } from "express";
+import { prisma } from "../config/db";
 
 export async function deleteContent(req: Request, res: Response) {
-  res.status(200).json({
-    message: "Delete Success",
-  });
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(403).json({
+        message: "provide ID",
+      });
+    }
+    const data = await prisma.content.delete({
+      where: { id },
+      select: {
+        type: true,
+        link: true,
+        title: true,
+      },
+    });
+    res.status(200).json({
+      message: "Delete Success",
+      data: data,
+    });
+  } catch (error) {
+    res.status(403).json({
+      message: "You Don't Own this shit",
+    });
+  }
 }
